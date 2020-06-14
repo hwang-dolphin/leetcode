a294c0c678fe1831d1b2f3a730137db019f45603
Robert <robert@Robertos-MacBook-Pro.local>
2020-06-14 10:45:03 -0700

    initial commit

---
+++ runningSumOf1dArray.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+#include <vector>
+
+vector<int> runningSum(vector<int>& nums)
+{
+    int acc = 0;
+    
+    vector<int> result;
+ 
+    for (int i = 0; i < nums.size(); i++)
+    {
+        acc += nums[i];
+        result.push_back(acc);
+    }
+
+    return result;
+}
+
+int main()
+{
+    vector<int> nums = {1,2,3,4};
+    vector<int> answer;
+    
+    answer = runningSum(nums);
+    
+    for (auto i: answer)
+    {
+        cout << i << " ";
+    }
+    
+    return 0;
+}
