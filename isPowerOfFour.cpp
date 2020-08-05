da8fc15c3cd18336fd0370662f1e0e9d23156eae
Robert <robert@Robertos-MacBook-Pro.local>
2020-08-05 00:22:50 -0700

    phase two

--- isPowerOfFour.cpp
+++ isPowerOfFour.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include <cmath>
 
 bool isPowerOfFour(int num);
+bool isPowerOfFourV2(int num);
 
 int main()
 {
@@ -30,3 +31,62 @@ bool isPowerOfFour(int num)
         
         return (result == (int) result) ? true : false;
 }
+
+
+bool isPowerOfFourV2(int num)
+{
+    // Using bit mask:  power of four has only one bit set at an even bit position (based zero)
+    // reject negative numbers
+
+    // runtime: O(1)
+    // space complexity: O(1) 
+
+    bool result = true;
+    
+    int leftMost;
+    int rightMost;
+    
+    
+    if (num < 0)
+    {
+        return false;
+    }
+    
+    for (int i = 0; i < 32; i++)
+    {
+        if (((1 << i) & num) != 0)
+        {
+            leftMost = i;
+            break;
+        }
+    }
+    
+    for (int i = 31; i >= 0; i--)
+    {
+        if (((1 << i) & num) != 0)
+        {
+            rightMost = i;
+            break;
+        }
+    }
+    
+    if (leftMost == rightMost)
+    {
+        for (int i = 1; i < 32; i = i + 2)
+        {
+            if (((1 << i) & num) != 0)
+            {
+                result = false;
+                break;
+            }
+        }
+    }
+    
+    else
+    {
+        result = false;
+    }
+
+    return result;
+}
+
